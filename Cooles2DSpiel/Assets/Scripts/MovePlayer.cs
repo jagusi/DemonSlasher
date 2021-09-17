5610da7f3e63fc346f991bf9afa9790d9e371153
Ali Ag <33959858+jagusi@users.noreply.github.com>
2021-09-17 19:19:44 +0200

    Leiter Eingebaut

--- Cooles2DSpiel/Assets/Scripts/MovePlayer.cs
+++ Cooles2DSpiel/Assets/Scripts/MovePlayer.cs
@@ -7,22 +7,26 @@ public class MovePlayer : MonoBehaviour
     [SerializeField] float jumpVelocity = 10f;
     [SerializeField] float walkSpeed = 5f;
     [SerializeField] private LayerMask platformsLayerMask;
+    [SerializeField] GameObject platformTileMap;
     SpriteRenderer spriteRenderer;
     Animator anim;
-    private BoxCollider2D boxCollider2d; 
-    private CapsuleCollider2D capsuleCollider2d;
     Rigidbody2D rb;
+    CapsuleCollider2D capsuleCollider2d;
+    Vector2 moveVector;
+    UnityEngine.Tilemaps.TilemapCollider2D platformTileCollider;
     float jumpValue;
     float moveHorizontal;
+    bool nearLadder = false;
     
     // Start is called before the first frame update
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        boxCollider2d = GetComponent<BoxCollider2D>();
         capsuleCollider2d = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        platformTileCollider = platformTileMap.GetComponent<UnityEngine.Tilemaps.TilemapCollider2D>();
+       
     }
     void Start()
     {
@@ -30,7 +34,7 @@ public class MovePlayer : MonoBehaviour
     }
     private void FixedUpdate()
     {
-
+        // Bewegung und Flip des Sprites 
            if (jumpValue == 1  && IsGrounded())
             {
              rb.velocity = new Vector2(0, jumpValue * jumpVelocity *Time.fixedDeltaTime);
@@ -50,19 +54,57 @@ public class MovePlayer : MonoBehaviour
             anim.SetBool("run", false);
             }
 
+           // Leiter nutzen
+        if (nearLadder == true)
+        {
+            Climb();
+        }
+
     }
     // Update is called once per frame
     void Update()
-    {            
+    {
+        GetInput();
+       
+    }
+    void GetInput()
+    {
         jumpValue = Input.GetAxisRaw("Jump");
         moveHorizontal = Input.GetAxisRaw("Horizontal");
+    }
+    void Climb()
+    {
+        Vector2 moveVector = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        rb.MovePosition(rb.position + moveVector * Time.fixedDeltaTime*2);
     }
     // Nur Springen wenn auf Platform ist 
     private bool IsGrounded()
     {
-       //RaycastHit2D raycastHit2d =  Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down , .1f, platformsLayerMask);
-        RaycastHit2D raycastHit2d =  Physics2D.BoxCast(boxCollider2d.bounds.center, capsuleCollider2d.bounds.size, 0f, Vector2.down , .1f, platformsLayerMask);
-        Debug.Log(raycastHit2d.collider);
+        RaycastHit2D raycastHit2d =  Physics2D.BoxCast(capsuleCollider2d.bounds.center, capsuleCollider2d.bounds.size, 0f, Vector2.down , .1f, platformsLayerMask);
        return raycastHit2d.collider != null; 
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            nearLadder = true;
+        }
+      
+        if(collision.name == "Platform Entry")
+        {
+            platformTileCollider.enabled = false;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ladder"))
+        {
+            nearLadder = false;
+        }
+        if (collision.name == "Platform Entry")
+        {
+            platformTileCollider.enabled = true;
+        }
+    }
+
 }
